ec2100b42d2554f66a004b7289bfe702b43cc475
chips556 <lohqihern@hotmail.com>
2022-02-11 21:21:06 +0800

    Game ends when head touches wall

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
+const width = 10;
 const squares = [];
 for (let i = 0; i < 100; i++) {
   squares.push(i);
@@ -11,9 +12,18 @@ function App() {
   const [direction, setDirection] = useState(1);
   const [snakePosition, setSnakePosition] = useState([2, 1, 0]);
 
+  const [gameStart, setGameStart] = useState(true);
+
   useEffect(() => {
-    const interval = setInterval(() => move(), 1000);
-    return () => clearInterval(interval);
+    if (gameStart) {
+      const interval = setInterval(() => move(), 1000);
+      return () => clearInterval(interval);
+    }
+  });
+
+  useEffect(() => {
+    const dom = document.addEventListener("keydown", changeDirection);
+    return () => dom.removeEventListener("keydown", changeDirection);
   }, []);
 
   function move() {
@@ -21,10 +31,50 @@ function App() {
     const snakePositionCopy = [...snakePosition];
     // take out last elem from arr
     const tail = snakePositionCopy.pop();
-    // add square in direction we are heading
-    snakePositionCopy.unshift(snakePosition[0] + direction);
-    // change original
-    setSnakePosition(snakePositionCopy);
+    // if game has not ended, add square in direction we are heading
+    if (!checkEndGame(snakePositionCopy)) {
+      snakePositionCopy.unshift(snakePosition[0] + direction);
+      setSnakePosition(snakePositionCopy);
+    }
+  }
+
+  function checkEndGame(arr) {
+    const head = arr[0];
+    if (
+      (head % width === 0 && direction === -1) || // left
+      (head % width === 9 && direction === 1) || // right
+      (head + width >= 100 && direction === width) ||
+      (head - width < 0 && direction === -width) ||
+      hasDuplicates(arr)
+    ) {
+      console.log("Touched a wall!");
+      setGameStart(false);
+      return true;
+    }
+  }
+
+  function changeDirection(e) {
+    switch (e.keyCode) {
+      case 37: // left
+        setDirection(-1);
+        break;
+      case 38: // up
+        setDirection(-width);
+        break;
+      case 39: // right
+        setDirection(1);
+        break;
+      case 40: // down
+        setDirection(width);
+        break;
+    }
+  }
+
+  // helper functions
+  function hasDuplicates(a) {
+    const noDups = new Set(a);
+
+    return a.length !== noDups.size;
   }
 
   return (
