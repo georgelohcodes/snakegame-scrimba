c61b3ba8d1cb6cf55f0c35a98e671855c4f6bfbf
chips556 <lohqihern@hotmail.com>
2022-02-11 11:08:33 +0800

    Snake can now move

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+
+const squares = [];
+for (let i = 0; i < 100; i++) {
+  squares.push(i);
+}
 
 function App() {
+  const [gridItems, setGridItems] = useState(() => squares);
+  const [direction, setDirection] = useState(1);
+  const [snakePosition, setSnakePosition] = useState([2, 1, 0]);
+
+  useEffect(() => {
+    const interval = setInterval(() => move(), 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  function move() {
+    // make copy
+    const snakePositionCopy = [...snakePosition];
+    // take out last elem from arr
+    const tail = snakePositionCopy.pop();
+    // add square in direction we are heading
+    snakePositionCopy.unshift(snakePosition[0] + direction);
+    // change original
+    setSnakePosition(snakePositionCopy);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="grid">
+        {gridItems.map((item) => (
+          <div
+            className={`square ${
+              snakePosition.includes(item) ? "snake-body" : ""
+            } ${snakePosition[0] === item ? "snake-head" : ""}`}
+          ></div>
+        ))}
+      </div>
     </div>
   );
 }
